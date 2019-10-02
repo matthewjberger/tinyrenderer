df26c632617abc0b055f6a4c5c8c5cf6bc80733e
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-10-02 15:50:18 -0700

    Load a model

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use image::{ImageBuffer, Pixel, Rgb};
-use std::mem;
+use std::{mem, path::Path};
+use tobj;
 
 type RendererImage = ImageBuffer<Rgb<u8>, Vec<u8>>;
 type RendererColor = Rgb<u8>;
 
 fn main() {
+    let model = tobj::load_obj(&Path::new("african_head.obj"));
     let (width, height) = (100, 100);
     let white = Rgb::from_channels(255, 255, 255, 255);
     let red = Rgb::from_channels(255, 0, 0, 255);
@@ -14,97 +16,6 @@ fn main() {
     line(80, 40, 13, 20, &mut img, red);
     img = image::imageops::flip_vertical(&img);
     img.save("output.png").unwrap();
-}
-
-// First attempt
-#[allow(dead_code)]
-fn line_1(x0: i32, y0: i32, x1: i32, y1: i32, img: &mut RendererImage, color: RendererColor) {
-    let resolution = 100;
-    for step in 0..resolution {
-        let t = step as f32 / resolution as f32;
-        let x = x0 + ((x1 - x0) as f32 * t) as i32;
-        let y = y0 + ((y1 - y0) as f32 * t) as i32;
-        img.put_pixel(x as u32, y as u32, color);
-        println!("{}, {}, {}", x, y, t);
-    }
-}
-
-// Second attempt
-#[allow(dead_code)]
-fn line_2(x0: i32, y0: i32, x1: i32, y1: i32, img: &mut RendererImage, color: RendererColor) {
-    for x in x0..=x1 {
-        let t = (x - x0) as f32 / (x1 - x0) as f32;
-        let y = (y0 as f32 * (1.0 - t) + y1 as f32 * t) as i32;
-        // equivalent to:
-        // let y = y0 + ((y1 - y0) as f32 * t) as i32;
-        img.put_pixel(x as u32, y as u32, color);
-        println!("{}, {}, {}", x, y, t);
-    }
-}
-
-// Third attempt
-#[allow(dead_code)]
-fn line_3(x0: i32, y0: i32, x1: i32, y1: i32, img: &mut RendererImage, color: RendererColor) {
-    let (mut x0, mut y0, mut x1, mut y1) = (x0, y0, x1, y1);
-    let steep = if (x0 - x1).abs() < (y0 - y1).abs() {
-        mem::swap(&mut x0, &mut y0);
-        mem::swap(&mut x1, &mut y1);
-        true
-    } else {
-        false
-    };
-
-    if x0 > x1 {
-        mem::swap(&mut x0, &mut x1);
-        mem::swap(&mut y0, &mut y1);
-    }
-
-    for x in x0..=x1 {
-        let t = (x - x0) as f32 / (x1 - x0) as f32;
-        let mut y = (y0 as f32 * (1.0 - t) + y1 as f32 * t) as i32;
-        let mut x = x;
-        if steep {
-            mem::swap(&mut x, &mut y);
-        }
-        img.put_pixel(x as u32, y as u32, color);
-        println!("{}, {}, {}", x, y, t);
-    }
-}
-
-// Fourth attempt
-#[allow(dead_code)]
-fn line_4(x0: i32, y0: i32, x1: i32, y1: i32, img: &mut RendererImage, color: RendererColor) {
-    let (mut x0, mut y0, mut x1, mut y1) = (x0, y0, x1, y1);
-    let steep = if (x0 - x1).abs() < (y0 - y1).abs() {
-        mem::swap(&mut x0, &mut y0);
-        mem::swap(&mut x1, &mut y1);
-        true
-    } else {
-        false
-    };
-
-    if x0 > x1 {
-        mem::swap(&mut x0, &mut x1);
-        mem::swap(&mut y0, &mut y1);
-    }
-
-    let dx = x1 - x0;
-    let dy = y1 - y0;
-    let derror = (dy as f32 / dx as f32).abs();
-    let mut error = 0.0;
-    let mut y = y0;
-    for x in x0..=x1 {
-        if steep {
-            img.put_pixel(y as u32, x as u32, color);
-        } else {
-            img.put_pixel(x as u32, y as u32, color);
-        }
-        error = error + derror;
-        if error > 0.5 {
-            y = y + if y1 > y0 { 1 } else { -1 };
-            error = error - 1.0;
-        }
-    }
 }
 
 fn line(x0: i32, y0: i32, x1: i32, y1: i32, img: &mut RendererImage, color: RendererColor) {
@@ -133,10 +44,10 @@ fn line(x0: i32, y0: i32, x1: i32, y1: i32, img: &mut RendererImage, color: Rend
         } else {
             img.put_pixel(x as u32, y as u32, color);
         }
-        error = error + derror;
+        error += derror;
         if error > dx as f32 {
-            y = y + if y1 > y0 { 1 } else { -1 };
-            error = error - dx as f32 * 2.0;
+            y += if y1 > y0 { 1 } else { -1 };
+            error -= dx as f32 * 2.0;
         }
     }
 }
